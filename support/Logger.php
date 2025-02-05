7de7b94437b5368dbe93e779806869ca48186e0d
落月 <1569097443@qq.com>
2025-02-05 15:07:27 +0800

    更换日志插件

---
+++ support/Logger.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace support;
+
+/**
+ * @method static void stdout($msg, string $type = 'info', array $context = [])
+ * @method static void app($msg, string $type = 'info', array $context = [])
+ * @method static void sql($msg, string $type = 'info', array $context = [])
+ */
+class Logger extends \WebmanTech\Logger\Logger
+{
+
+}
